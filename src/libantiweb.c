5cfe874db30b6fcf381f7320f941f234e991a03e
doug <doug@eclipse.(none)>
2010-01-17 12:06:36 -0500

    If no logger process is connected, print log messages to STDERR instead of STDOUT

--- src/libantiweb.c
+++ src/libantiweb.c
@@ -94,7 +94,7 @@ void aw_log(char *file, char *prefix, char *log_msg) {
   size_t len, plen, mlen;
 
   if (logger_conn == NULL) {
-    printf("%s%s\n", prefix, log_msg);
+    fprintf(STDERR, "%s%s\n", prefix, log_msg);
     return;
   }
 

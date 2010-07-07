c9a20868d0bfef7bc99aff55ab16249223a8cf40
doug <doug@pulsar.(none)>
2010-07-07 04:17:24 -0400

    start of deb packaging infrastructure

---
+++ package/make-debian-package.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use strict;
+
+my $bits = length pack("l!")==8 ? 64 : 32;
+
+my $tag = `git describe --tags --match $product-\*`;
+
+print "QQQ: $tag\n";
+
+
+die "Unable to find DEBIAN/ directory" unless -d 'DEBIAN';
+
+sys("rm -rf build");
+sys("cp -R DEBIAN build");
+
+sys("mkdir -p build/usr/local/antiweb");
+
+sys("mkdir -p build/usr/local/antiweb");
+
+
+
+sub sys {
+  my $cmd = shift;
+  print "SYSTEM: $cmd\n";
+  my $ret = system($cmd);
+  die "** system() failed with non-zero exit code ($ret)" if $ret;
+}
+
+
+=pod
+/usr/local/antiweb/
+  lib
+
+  bdb32
+  bdb64
+
+  ccl
+=cut
